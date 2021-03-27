1e33f2bc02cbf0196468743e0d588d62acc4817a
xuubey <krasny.sylwia@gmail.com>
2021-03-27 13:10:23 +0100

    showError&clearError

--- script.js
+++ script.js
@@ -6,6 +6,39 @@ const sendBtn = document.querySelector('.send');
 const clearBtn = document.querySelector('.clear');
 const popup = document.querySelector('.popup');
 
+const showError = (input, msg) => {
+
+    const formBox = input.parentElement;
+    const errorMsg = formBox.querySelector('.error-text');
+
+    formBox.classList.add('error');
+    errorMsg.textContent = msg;
+
+}
+
+const clearError = input => {
+    const formBox = input.parentElement;
+    formBox.classList.remove('error');
+
+}
+
+const checkForm = input => {
+    input.forEach(el => {
+        if(el.value === ''){
+            showError(el, el.placeholder);
+        } else {
+            clearError(el);
+        }
+    })
+
+}
+
+sendBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    checkForm([username, pass, pass2, email])
+
+})
 
 clearBtn.addEventListener('click', e => {
 e.preventDefault();
@@ -14,5 +47,4 @@ e.preventDefault();
     el.value = '';
 })
 
-
-})
+}) 
