669669c15f98cd1690c56d4c76f53b54ac63d0db
xuubey <krasny.sylwia@gmail.com>
2021-03-30 17:20:54 +0200

    validating email

--- script.js
+++ script.js
@@ -43,7 +43,17 @@ const checkPassword = (pass1, pass2) => {
     if(pass1.value !== pass2.value){
         showError(pass2, 'Hasła do siebie nie pasują!')
     }
+}
 
+const checkMail = email => {
+
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if(re.test(email.value)){
+            clearError(email)
+        } else {
+            showError(email, 'Adres email jest niepoprawny!')
+        }
 }
 
 sendBtn.addEventListener('click', e => {
@@ -52,7 +62,8 @@ sendBtn.addEventListener('click', e => {
     checkForm([username, pass, pass2, email]);
     checkLength(username, 3);
     checkLength(pass, 8);
-    checkPassword(pass, pass2)
+    checkPassword(pass, pass2);
+    checkMail(email);
 
 })
 
